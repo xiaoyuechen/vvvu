3c4cba32cba17d9ef5dbaf3f4b527269d9e85df5
Xiaoyue Chen <xchen@vvvu.org>
2024-06-26 22:18:38 +0200

    Add nextcloud.vvvu.org

--- flake.nix
+++ flake.nix
@@ -5,21 +5,26 @@
     nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
   };
 
-  outputs = { self, nixpkgs, nixos-mailserver }: {
-    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+  outputs = { self, nixpkgs, nixos-mailserver }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; }; in
+    {
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
 
-    packages.x86_64-linux.digital-ocean-image =
-      with import nixpkgs { system = "x86_64-linux"; };
-      (nixos {
-        imports = [ (nixpkgs + "/nixos/modules/virtualisation/digital-ocean-image.nix") ];
-      }).digitalOceanImage;
+      packages.x86_64-linux.digital-ocean-image =
+        (pkgs.nixos {
+          imports = [ (pkgs + "/nixos/modules/virtualisation/digital-ocean-image.nix") ];
+        }).digitalOceanImage;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.digital-ocean-image;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.digital-ocean-image;
 
-    nixopsConfigurations.default = import ./vvvu.nix {
-      pkgs = nixpkgs;
-      nixos-mailserver = nixos-mailserver;
-      keys = import ./keys.nix;
+      devShells.x86_64-linux.default = with pkgs; mkShell {
+        packages = [ nixops_unstable_minimal ];
+      };
+
+      nixopsConfigurations.default = import ./vvvu.nix {
+        inherit nixpkgs;
+        nixos-mailserver = nixos-mailserver;
+        keys = import ./keys.nix;
+      };
     };
-  };
 }
--- vvvu.nix
+++ vvvu.nix
@@ -1,10 +1,10 @@
-{ pkgs
+{ nixpkgs
 , nixos-mailserver
 , keys
 }:
 
 {
-  nixpkgs = pkgs;
+  inherit nixpkgs;
 
   network = {
     description = "vvvu.org";
@@ -12,7 +12,7 @@
   };
 
   defaults = {
-    imports = [ (pkgs + "/nixos/modules/virtualisation/digital-ocean-image.nix") ];
+    imports = [ (nixpkgs + "/nixos/modules/virtualisation/digital-ocean-image.nix") ];
 
     security.acme = {
       acceptTerms = true;
@@ -69,48 +69,28 @@
     networking.firewall.allowedTCPPorts = [ 80 443 ];
   };
 
-  wg = { config, pkgs, ... }: {
-    deployment.targetHost = "wg.vvvu.org";
-    deployment.keys.wg0.text = keys.wg.wg0.private;
+  nextcloud = { config, pkgs, ... }: {
+    deployment.targetHost = "nextcloud.vvvu.org";
+    deployment.keys.admin = {
+      text = keys.nextcloud;
+      user = "nextcloud";
+      group = "nextcloud";
+    };
 
-    networking.nat = {
+    users.groups.keys.members = [ "nextcloud" ];
+
+    services.nextcloud = {
       enable = true;
-      externalInterface = "ens3";
-      internalInterfaces = [ "wg0" ];
+      package = pkgs.nextcloud29;
+      hostName = "nextcloud.vvvu.org";
+      config.adminpassFile = "/run/keys/admin";
     };
 
-    networking.firewall = {
-      allowedTCPPorts = [ 53 ];
-      allowedUDPPorts = [ 53 51820 ];
+    services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+      forceSSL = true;
+      enableACME = true;
     };
 
-
-    networking.wireguard.interfaces.wg0 = {
-      privateKeyFile = "/run/keys/wg0";
-
-      ips = [ "10.100.0.1/24" ];
-      listenPort = 51820;
-
-      postSetup = "${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE";
-      postShutdown = "${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE";
-
-      peers = [
-        {
-          name = "racc";
-          publicKey = keys.wg.wg0.peers.racc.public;
-          allowedIPs = [ "10.100.0.2/32" ];
-        }
-        {
-          name = "galaxy";
-          publicKey = keys.wg.wg0.peers.galaxy.public;
-          allowedIPs = [ "10.100.0.3/32" ];
-        }
-      ];
-    };
-
-    services.dnsmasq = {
-      enable = true;
-      extraConfig = "interface=wg0";
-    };
+    networking.firewall.allowedTCPPorts = [ 80 443 ];
   };
 }
